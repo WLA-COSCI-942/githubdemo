eca13e598048142db5443d3d9c48161ac69240f7
Surinder Singh <surindersingh9495@gmail.com>
2018-11-07 20:14:10 -0800

    added another line

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main() {
-    cout << "Hello, Discrete Math! \n"
+    cout << "Hello, Discrete Math! \n" << endl;
+    cout << "Hi from Surinder" << endl;
     return 1;
 }
